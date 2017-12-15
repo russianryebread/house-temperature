ca1f770c7b2f8f0aec8321717fd528def789c983
Ryan Hoshor <ryan@farmsoftstudios.com>
2017-12-15 16:23:31 -0600

    Add history collection

---
+++ db.js
@@ -0,0 +1,56 @@
+const sqlite3 = require('sqlite3').verbose();
+
+const TABLE_NAME = 'historic_temps'
+const DB_NAME = 'temps.db'
+
+class Db
+{
+    constructor()
+    {
+        this.db = new sqlite3.Database(`./db/${DB_NAME}`);
+        this.up()
+    }
+
+    up() {
+        this.db.run(`CREATE TABLE IF NOT EXISTS ${TABLE_NAME} (temp, created_at)`);
+    }
+
+    historic() {
+        let results = []
+
+        let sql = `SELECT temp, created_at FROM ${TABLE_NAME} ORDER BY created_at ASC LIMIT 100`;        
+        this.db.each(sql, ['w'], (err, row) => {
+            if (err) throw err
+            results.push(row)
+        })
+
+        return results
+    }
+
+    save(temp) {
+        let response = {
+            success: true,
+            message: "Updated!"
+        }
+        let sql = `INSERT INTO ${TABLE_NAME}(temp, created_at)`
+        let date = new Date().toISOString()
+
+        this.db.run(sql, [temp, date], function(err) {
+            if (err) {
+                console.error(err.message)
+                response.success = false
+                response.message = `Error saving temp: ${err.message}`
+                return response
+            }
+        })
+        
+        return response
+    }
+
+
+    destroy() {
+        this.db.close();
+    }
+}
+
+module.exports = Db
--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 const thermometers = require('temper1')
+const Db = require('./db')
 const utils = require('./utils.js')
 const cors = require('cors')
 const express = require('express')
 const app = express()
+const db = new Db()
 
-const port = 80
+const port = 8000
 
 app.set('view engine', 'pug')
 app.use(express.static('public'))
 app.use(cors())
 
-var devices = thermometers.getDevices()
+let devices = thermometers.getDevices()
 console.log(`Devices found: ${devices}`)
 
 app.get('/', (req, res) => {
@@ -29,16 +31,20 @@ app.get('/', (req, res) => {
 
 })
 
-
 app.get('/api', (req, res) => {
-    
     thermometers.readTemperature(devices[0], (err, c) => {
         res.json({
             c: utils.round(c),
             f: utils.round(thermometers.celsiusToFahrenheit(c)),
+            history: db.historic()
         })
-    });
+    })
+})
 
+app.post('/api/save', (req, res) => {
+    thermometers.readTemperature(devices[0], (err, c) => {
+        res.json(db.save(thermometers.celsiusToFahrenheit(c)))
+    })
 })
 
 app.listen(port, () => console.log(`Temperature app listening on port ${port}!`))
