1f4a88d8f9a68fd790a9be4fc6c18b34bff3b551
Ryan Hoshor <ryan@hoshor.me>
2018-03-11 06:26:51 +0000

    use Env vars

--- push.js
+++ push.js
@@ -1,13 +1,13 @@
 const Pusher = require('pusher')
 const pusher = new Pusher({
-    appId: '489906',
-    key: 'e00904f9d5e6bca9fa0c',
-    secret: 'f1bf8a6a81024b78a3c6',
+    appId: process.env.PUSHER_APP_ID,
+    key: process.env.PUSHER_KEY,
+    secret: PUSHER_SECRET,
     cluster: 'us2',
     encrypted: true
 });
 
-const channel = 'house'
+const channel = process.env.PUSHER_CHANNEL
 
 exports.event = (event, message, data = {}) => {
     pusher.trigger(channel, event, {
