c6fceba7ed9b16a8198b742781041fd46da62f83
Ryan Hoshor <ryan@hoshor.me>
2018-03-11 15:36:02 +0000

    commit html

--- push.js
+++ push.js
@@ -2,7 +2,7 @@ const Pusher = require('pusher')
 const pusher = new Pusher({
     appId: process.env.PUSHER_APP_ID,
     key: process.env.PUSHER_KEY,
-    secret: PUSHER_SECRET,
+    secret: process.env.PUSHER_SECRET,
     cluster: 'us2',
     encrypted: true
 });
